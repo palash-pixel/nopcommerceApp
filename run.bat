d5405ea87066e14082635537282def0138b5c533
palash-pixel <85416162+palash-pixel@users.noreply.github.com>
2021-06-24 23:45:10 -0400

    test data commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
- rerem pytest -vs -m "regression" --browser chrome
+pytest -vs -m "regression" --browser chrome
 rem pytest -vs -m "sanity"--browser chrome
 
 rem pytest -vsk log  testCases/test_login.py --browser chrome
